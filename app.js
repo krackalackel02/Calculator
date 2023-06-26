09680fd68a454bddafc6b37e4bbdc320d800d355
Paul Kallarackel <108292404+krackalackel02@users.noreply.github.com>
2023-06-26 17:59:14 +0100

    Added input number and AC functionality still need to do other buttons and majority of logic and adding commas to display

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
+let currentOp = document.getElementById("current-operand");
+let previousOp = document.getElementById("previous-operand");
 
+let previousSum = previousOp.innerHTML;
+
+let operation = "return " + previousSum;
+let evaluator = Function(undefined, operation);
+
+console.log(evaluator());
+
+let buttons = Array.from(document.querySelectorAll(".btn"));
+buttons.forEach((button) => {
+	button.addEventListener("click", (e) => {
+		let numeric = e.target.innerHTML;
+		if (e.button !== 0) return;
+		switch (numeric) {
+			case "AC":
+				previousOp.innerHTML = "";
+				currentOp.innerHTML = "";
+				break;
+			case "±":
+				break;
+			case "%":
+				break;
+			case "÷":
+				break;
+			case "×":
+				break;
+			case "−":
+				break;
+			case "+":
+				break;
+			case "=":
+				break;
+			case ".":
+				break;
+			default:
+				currentOp.innerHTML+=numeric;
+		}
+        currentOp.innerHTML = addCommasToNumber(currentOp.innerHTML)
+	});
+});
+
+
+// function addCommasToNumber(numberString) {
+//     // Match any sequence of three digits followed by a non-digit character
+//     const regex = /\B(?=(\d{3})+(?!\d))/g;
+  
+//     // Insert commas after every match
+//     return numberString.replace(regex, ",");
+//   }
